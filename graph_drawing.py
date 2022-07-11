3182bffce8f71e7d21e08c32d53ef431a59aed85
Ennio Malvati <enniohomero@hotmail.it>
2022-07-11 17:39:16 +0200

    added label with composed string

--- graph_drawing.py
+++ graph_drawing.py
@@ -342,8 +342,155 @@ def drawing_2D_interactive():
     fig.update_layout(yaxis=dict(scaleanchor="x", scaleratio=1.3), plot_bgcolor='rgb(255,255,255)')
     fig.update_layout(uniformtext_minsize=3)
     fig.update_traces(mode="markers+text")
-    #fig.show()
+    # fig.show()
     py.plot(fig)
 
 
 drawing_2D_interactive()
+
+
+def drawing_3D():
+    G = nx.MultiDiGraph()
+    G.add_nodes_from(lista_nodi)
+    lista_archi = load_edges()
+    for nodo in lista_archi:
+        G.add_edge(nodo[0], nodo[1])
+
+    random_layout = nx.random_layout(G, dim=3, seed=20)
+    spring_3D = nx.spring_layout(G, dim=3, seed=18, pos=random_layout, scale=10, k=2)
+
+    Num_nodes = 43
+
+    # we need to seperate the X,Y,Z coordinates for Plotly
+    x_nodes = [spring_3D[str(i)][0] for i in range(1, Num_nodes)]  # x-coordinates of nodes
+    y_nodes = [spring_3D[str(i)][1] for i in range(1, Num_nodes)]  # y-coordinates
+    z_nodes = [spring_3D[str(i)][2] for i in range(1, Num_nodes)]  # z-coordinates
+
+    x_neutri = list()
+    y_neutri = list()
+    z_neutri = list()
+
+    for i in range(1, Num_nodes):
+        if str(i) in dizionario_neutri.keys():
+            x_neutri.append(spring_3D[str(i)][0])
+            y_neutri.append(spring_3D[str(i)][1])
+            z_neutri.append(spring_3D[str(i)][2])
+
+    x_maschi = list()
+    y_maschi = list()
+    z_maschi = list()
+    for i in range(1, Num_nodes):
+        if str(i) in dizionario_maschi.keys():
+            x_maschi.append(spring_3D[str(i)][0])
+            y_maschi.append(spring_3D[str(i)][1])
+            z_maschi.append(spring_3D[str(i)][2])
+
+    x_femmine = list()
+    y_femmine = list()
+    z_femmine = list()
+    for i in range(1, Num_nodes):
+        if str(i) in dizionario_femmine.keys():
+            x_femmine.append(spring_3D[str(i)][0])
+            y_femmine.append(spring_3D[str(i)][1])
+            z_femmine.append(spring_3D[str(i)][2])
+
+    edge_list = G.edges()
+    edge_list
+
+    # we  need to create lists that contain the starting and ending coordinates of each edge.
+    x_edges = []
+    y_edges = []
+    z_edges = []
+
+    # need to fill these with all of the coordiates
+    for edge in edge_list:
+        # format: [beginning,ending,None]
+        x_coords = [spring_3D[edge[0]][0], spring_3D[edge[1]][0], None]
+        x_edges += x_coords
+
+        y_coords = [spring_3D[edge[0]][1], spring_3D[edge[1]][1], None]
+        y_edges += y_coords
+
+        z_coords = [spring_3D[edge[0]][2], spring_3D[edge[1]][2], None]
+        z_edges += z_coords
+
+    node_labels = load_nodes_label()
+
+    trace_edges = go.Scatter3d(x=x_edges,
+                               y=y_edges,
+                               z=z_edges,
+                               mode='text',
+                               line=dict(color='black', width=2),
+                               #text=load_edges_label(),
+                               hoverinfo='text')
+
+    trace_nodes = go.Scatter3d(x=x_nodes,
+                               y=y_nodes,
+                               z=z_nodes,
+                               mode='markers+lines',
+                               opacity=1,
+                               marker=dict(symbol='circle',
+                                           size=8,
+                                           color=lista_colori,
+                                           line=dict(color='black', width=0.9)),
+                               #text=node_labels,
+                               hoverinfo='text')
+
+    nodi_maschi = go.Scatter3d(x=x_maschi,
+                               y=y_maschi,
+                               z=z_femmine,
+                               mode='text',
+                               marker=dict(symbol='circle',
+                                           size=6,
+                                           color='#3776ab',
+                                           line=dict(color='black', width=0.9)
+                                           ),
+                               hoverinfo='text')
+
+    nodi_femmina = go.Scatter3d(x=x_femmine,
+                                y=y_femmine,
+                                z=z_femmine,
+                                mode='text',
+                                marker=dict(symbol='circle',
+                                            size=10,
+                                            color='#76ab37',
+                                            line=dict(color='black', width=0.9)
+                                            ),
+                                hoverinfo='text')
+
+    nodi_neutri = go.Scatter3d(x=x_neutri,
+                               y=y_neutri,
+                               z=z_neutri,
+                               mode='text',
+                               marker=dict(symbol='circle',
+                                           size=10,
+                                           color='#ab3776',
+                                           line=dict(color='#676767', width=0.9)
+                                           ),
+                               hoverinfo='text')
+
+    axis = dict(showbackground=True,
+                showline=False,
+                zeroline=False,
+                showgrid=True,
+                showticklabels=False,
+                title='')
+
+    # also need to create the layout for our plot
+    layout = go.Layout(title="Rappresentazione 3D della saga Hrafnkel",
+                       width=650,
+                       height=625,
+                       showlegend=False,
+                       scene=dict(xaxis=dict(axis),
+                                  yaxis=dict(axis),
+                                  zaxis=dict(axis),
+                                  ),
+                       margin=dict(t=100),
+                       hovermode='closest')
+
+    # Include the traces we want to plot and create a figure
+    data = [trace_edges, trace_nodes, nodi_neutri, nodi_maschi, nodi_femmina]
+    fig = go.Figure(data=data, layout=layout)
+    fig.show()
+
+drawing_3D()
